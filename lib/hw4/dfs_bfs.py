fce1777e00882d2ea84659c4855acbcd02c1594f
nisunalade <nisunalade13@gmail.com>
2019-04-15 21:30:36 -0700

    HW 4-1

--- lib/hw4/dfs_bfs.py
+++ lib/hw4/dfs_bfs.py
@@ -10,25 +10,42 @@ class Graph:
 
 	def dfs(self, v):
 		# mark all vertex to not marked
-		# recurse marked function
+		n = len(self.graph)
+		marked = [False]* n
 
+		# recurse marked function
+		self. dfs_marked(v, marked)
 		return 1
 
 	def dfs_marked(self, v, marked):
-		marked[v] = True #current node is visited
+		marked[v] = True  #current node is visited
 
-		#for i in self.graph[v]:
+		for i in self.graph[v]:
+			if marked[i] == False:
+				self.dfs_marked(i, marked) #recursive
+
 		#if the point has not been marked then go and mark it true
-
 
 
 	def bfs(self, v):
 		# mark all vertex to not marked
+		n = len(self.graph)
+		marked = [False] * n
+
 		# create queue
+		Q = []
+
 		# mark the origin and enqueue it
+		marked[v] = True
+		Q.append(v)
+
 		#while queue: dequeue and update by getting all adjacent values that
 		#are not marked and mark and enqueue them
-
-
+		while Q:
+			v = Q.pop(0)
+			for i in self.graph[v]:
+				if marked[i] == False:
+					Q.append(i)
+					marked[i] = True
 		return 1
 
